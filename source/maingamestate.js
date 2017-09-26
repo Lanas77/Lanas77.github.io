cbf99b2e905f167c58a90f5d00b20785a0959217
Lanas77 <linda.antonsson@lionize.se>
2017-09-26 15:32:29 +0200

    added maingamestate

---
+++ source/maingamestate.js
@@ -0,0 +1,16 @@
+var mainGameState = {}
+    
+//add the preload function
+mainGameState.preload = function() {
+    console.log("Pre-loading the Game");
+}
+
+//add the create function
+mainGameState.create = function() {
+
+}
+
+//Add the update function
+mainGameState.update = function() {
+    
+}
