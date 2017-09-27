096e80ad26c1bdc8c58ccffc44820b2495ebe6a4
Lanas77 <linda.antonsson@lionize.se>
2017-09-27 11:02:15 +0200

    enabled phaser arcade physics

--- source/maingamestate.js
+++ source/maingamestate.js
@@ -9,14 +9,22 @@ mainGameState.preload = function() {
 
 //add the create function 
 mainGameState.create = function() {
-    game.add.sprite(-100, -200, 'space-bg');
     
-var x = game.width * 0.5;
-var y = game.height * 0.95;
+    var x = game.width * 0.5;
+    var y = game.height * 0.95;
+   
+    //Background
+    this.game.add.sprite(-100, -200, 'space-bg');
 
+    //Spaceship
     this.playerShip = game.add.sprite(x, y, 'playership');
     this.playerShip.anchor.setTo(0.5, 0.5);
     this.playerShip.scale.setTo(0.5,0.5);
+    
+    //Move
+    game.physics.startSystem(Phaser.Physics.ARCADE); 
+    game.physics.arcade.enable(this.playerShip);
+    
 }
 
 //Add the update function
