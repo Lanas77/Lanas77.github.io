35c4210329ca9de37e78f7f91ffd48a2aa5437ad
Lanas77 <linda.antonsson@lionize.se>
2017-09-27 10:41:28 +0200

    changed spaceship

--- source/maingamestate.js
+++ source/maingamestate.js
@@ -4,7 +4,7 @@ var mainGameState = {}
 mainGameState.preload = function() {
     console.log("Pre-loading the Game");
     this.game.load.image("space-bg", "assets/images/space2-bg.jpg");
-    this.game.load.image("playership", "assets/images/player-ship.png");
+    this.game.load.image("playership", "assets/images/alien4.png");
 }
 
 //add the create function 
@@ -12,11 +12,11 @@ mainGameState.create = function() {
     game.add.sprite(-100, -200, 'space-bg');
     
 var x = game.width * 0.5;
-var y = game.height * 0.9;
+var y = game.height * 0.95;
 
     this.playerShip = game.add.sprite(x, y, 'playership');
     this.playerShip.anchor.setTo(0.5, 0.5);
-    //this.playerShip.scale.setTo(-0.5,-0.5);
+    this.playerShip.scale.setTo(0.5,0.5);
 }
 
 //Add the update function
