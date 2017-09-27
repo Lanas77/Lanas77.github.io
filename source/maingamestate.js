ed59b878f1e72f6151dc7b0b226e45b5c7e8af45
Lanas77 <linda.antonsson@lionize.se>
2017-09-27 10:21:07 +0200

    added bg and spaceship

--- source/maingamestate.js
+++ source/maingamestate.js
@@ -3,11 +3,20 @@ var mainGameState = {}
 //add the preload function
 mainGameState.preload = function() {
     console.log("Pre-loading the Game");
+    this.game.load.image("space-bg", "assets/images/space2-bg.jpg");
+    this.game.load.image("playership", "assets/images/player-ship.png");
 }
 
-//add the create function
+//add the create function 
 mainGameState.create = function() {
+    game.add.sprite(-100, -200, 'space-bg');
+    
+var x = game.width * 0.5;
+var y = game.height * 0.9;
 
+    this.playerShip = game.add.sprite(x, y, 'playership');
+    this.playerShip.anchor.setTo(0.5, 0.5);
+    //this.playerShip.scale.setTo(-0.5,-0.5);
 }
 
 //Add the update function
