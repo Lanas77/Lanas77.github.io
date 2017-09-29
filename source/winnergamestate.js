46ccf7679a9cdffb84f313dde9a93884f25f8b2f
Lanas77 <linda.antonsson@lionize.se>
2017-09-29 14:05:32 +0200

    began winnerscreen startover

--- source/winnergamestate.js
+++ source/winnergamestate.js
@@ -1,7 +1,7 @@
 var winnerGameState = {}
 
 winnerGameState.preload = function (){
-    game.load.image("winnerbg", "assets/images/winnerbg.jpeg");
+    
     
     
 }
@@ -10,12 +10,23 @@ winnerGameState.preload = function (){
 
 
 winnerGameState.create = function () {
-    game.add.sprite(0, 0, 'winnerbg');
-    
+    var textStyle1 = {font: "50px Arial", fill: "#43F723", align: "center"};
+     var textStyle2 = {font: "30px Arial", fill: "#ffffff", align: "center"};
+
+	WinnerTitle = game.add.text(game.width * 0.3, 40, "YOU WON!", textStyle1);
+
+    WinnerText = game.add.text(game.width *0.15,130, "Wanna play again? Click any key", textStyle2);
 }
 
 
 winnerGameState.update = function () {
+    
+    //var startoverkey = "Q"
+   // if (startoverkey = true){
+        //game.state.start("MainGame");
+   // } else {
+       // null
+   // }
     
     
 }
